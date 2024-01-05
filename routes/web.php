c2907c5e83be12cf305f2363c2187edf4bb77c39
kamrulcse404 <kamrul.h01960@gmail.com>
2024-01-05 18:32:24 +0600

    create user dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminDashboardContraoller;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -29,3 +30,7 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+
+Route::get('admin/dashboard', [AdminDashboardContraoller::class, 'index'])->middleware('auth')->name('admin.dashboard');
+
