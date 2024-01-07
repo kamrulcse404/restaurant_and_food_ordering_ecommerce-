5c5e8f3df105e9b84408fd8d9ab1832d1e6bc025
kamrulcse404 <kamrul.h01960@gmail.com>
2024-01-07 14:42:11 +0600

    frontend reset password page mastering

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,29 +1,4 @@
-{{-- <x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-    </div>
 
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required
-                autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout> --}}
 
 
 @extends('frontend.layouts.master')
@@ -38,7 +13,7 @@
                 <div class="fp__breadcrumb_text">
                     <h1>forgot password</h1>
                     <ul>
-                        <li><a href="index.html">home</a></li>
+                        <li><a href="javascript:;">home</a></li>
                         <li><a href="#">forgot password</a></li>
                     </ul>
                 </div>
