ff9626ac20c5ff7f8147d468c543bf28be2dbfc4
kamrulcse404 <kamrul.h01960@gmail.com>
2024-01-07 15:09:51 +0600

    admin login page mastering

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -6,27 +6,27 @@
             content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no"
             name="viewport"
         />
-        <title>Login &mdash; Stisla</title>
+        <title>Login</title>
 
         <!-- General CSS Files -->
         <link
             rel="stylesheet"
-            href="assets/modules/bootstrap/css/bootstrap.min.css"
+            href="{{ asset('admin/assets/modules/bootstrap/css/bootstrap.min.css') }}"
         />
         <link
             rel="stylesheet"
-            href="assets/modules/fontawesome/css/all.min.css"
+            href="{{ asset('admin/assets/modules/fontawesome/css/all.min.css') }}"
         />
 
         <!-- CSS Libraries -->
         <link
             rel="stylesheet"
-            href="assets/modules/bootstrap-social/bootstrap-social.css"
+            href="{{ asset('admin/assets/modules/bootstrap-social/bootstrap-social.css') }}"
         />
 
         <!-- Template CSS -->
-        <link rel="stylesheet" href="assets/css/style.css" />
-        <link rel="stylesheet" href="assets/css/components.css" />
+        <link rel="stylesheet" href="{{ asset('admin/assets/css/style.css') }}" />
+        <link rel="stylesheet" href="{{ asset('admin/assets/css/components.css') }}" />
         <!-- Start GA -->
         <script
             async
@@ -188,20 +188,20 @@
         </div>
 
         <!-- General JS Scripts -->
-        <script src="assets/modules/jquery.min.js"></script>
-        <script src="assets/modules/popper.js"></script>
-        <script src="assets/modules/tooltip.js"></script>
-        <script src="assets/modules/bootstrap/js/bootstrap.min.js"></script>
-        <script src="assets/modules/nicescroll/jquery.nicescroll.min.js"></script>
-        <script src="assets/modules/moment.min.js"></script>
-        <script src="assets/js/stisla.js"></script>
+        <script src="{{ asset('admin/assets/modules/jquery.min.js') }}"></script>
+        <script src="{{ asset('admin/assets/modules/popper.js') }}"></script>
+        <script src="{{ asset('admin/assets/modules/tooltip.js') }}"></script>
+        <script src="{{ asset('admin/assets/modules/bootstrap/js/bootstrap.min.js') }}"></script>
+        <script src="{{ asset('admin/assets/modules/nicescroll/jquery.nicescroll.min.js') }}"></script>
+        <script src="{{ asset('admin/assets/modules/moment.min.js') }}"></script>
+        <script src="{{ asset('admin/assets/js/stisla.js') }}"></script>
 
         <!-- JS Libraies -->
 
         <!-- Page Specific JS File -->
 
         <!-- Template JS File -->
-        <script src="assets/js/scripts.js"></script>
-        <script src="assets/js/custom.js"></script>
+        <script src="{{ asset('admin/assets/js/scripts.js') }}"></script>
+        <script src="{{ asset('admin/assets/js/custom.js') }}"></script>
     </body>
 </html>
