52b4fe1f37c5d3fa045b6c26969e00aea197fa9c
kamrulcse404 <kamrul.h01960@gmail.com>
2024-01-12 17:37:42 +0600

    global function for iamge upload

---
+++ app/Traits/FileUploadTrait.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Traits;
+
+use Request;
+
+trait FileUploadTrait
+{
+    function uploadImage(Request $request, $inputName, $path="/uploads"){
+        if ($request->hasFile($inputName)) {
+            $image = $request->{$inputName};
+            $ext = $image->getClientOriginalExtention();
+            $imageName = 'media_'.uniqid().'.'.$ext;
+
+            $image->move(public_path($path));
+
+            return $path.'/'.$imageName; 
+        }
+    }
+}
