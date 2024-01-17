8df0e548f541315fb5845c63a7083d2573ce15fc
kamrulcse404 <kamrul.h01960@gmail.com>
2024-01-17 20:35:42 +0600

    frontend mastering

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\AdminAuthController;
+use App\Http\Controllers\Frontend\DashboardController;
 use App\Http\Controllers\Frontend\FrontendController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
@@ -21,9 +22,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('admin/login', [AdminAuthController::class, 'index'])->name('admin.login');
 
-Route::get('/dashboard', function(){
-    return view('dashboard');
-})->name('dashboard');
+Route::group(['middleware' => 'auth'], function(){
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+});
 
 Route::get('/', [FrontendController::class, 'index'])->name('home');
 
